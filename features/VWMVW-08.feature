5e7aadb9d7c2c40a29a3a2140ec4fc5dee7fa7f9
Sakari Ruoho <finnishandy@gmail.com>
2015-08-11 17:11:04 +0100

    more features

--- features/VWMVW-08.feature
+++ features/VWMVW-08.feature
@@ -1,6 +1,21 @@
 Feature: [VWMVW-08] - Verify that adding an owned car behaves as expected
 
+  @medium
   Scenario: Login and open the add a car page (either directly or by executing a previous test)
     Given Log in the VW account
     When user opens the add a car page
     Then The page should load
+
+  Scenario: Observe the contents of the page
+    Given user is on add a car main page
+    Then the page should contain the text "I'd like to add"
+    And button A car I won
+    And button A car I configured
+    And button A configured car
+
+  Scenario: Select "A car I own" and observe the modifications from the page
+    Given user is on add a car main page
+    When user selects A car I own
+    Then the text "My registration number" should be shown
+    And an edit box should be shown
+    And next button should be enabled
--- features/step_definitions/VWMVW-08.Definitions.js
+++ features/step_definitions/VWMVW-08.Definitions.js
@@ -2,6 +2,7 @@ var aTest = function () {
 	this.World = require("../support/world.js").World;
 
 
+	/** STEP 1 **/
 	this.When(/^user opens the add a car page$/, function (callback) {
 		// Write code here that turns the phrase above into concrete actions
 		callback.pending();
@@ -11,6 +12,52 @@ var aTest = function () {
 		// Write code here that turns the phrase above into concrete actions
 		callback.pending();
 	});
+
+	this.Given(/^user is on add a car main page$/, function (callback) {
+		// Write code here that turns the phrase above into concrete actions
+		callback.pending();
+	});
+
+	this.Then(/^the page should contain the text "([^"]*)"$/, function (arg1, callback) {
+		// Write code here that turns the phrase above into concrete actions
+		callback.pending();
+	});
+
+	this.Then(/^button A car I won$/, function (callback) {
+		// Write code here that turns the phrase above into concrete actions
+		callback.pending();
+	});
+
+	this.Then(/^button A car I configured$/, function (callback) {
+		// Write code here that turns the phrase above into concrete actions
+		callback.pending();
+	});
+
+	this.Then(/^button A configured car$/, function (callback) {
+		// Write code here that turns the phrase above into concrete actions
+		callback.pending();
+	});
+
+	this.When(/^user selects A car I own$/, function (callback) {
+		// Write code here that turns the phrase above into concrete actions
+		callback.pending();
+	});
+
+	this.Then(/^the text "([^"]*)" should be shown$/, function (arg1, callback) {
+		// Write code here that turns the phrase above into concrete actions
+		callback.pending();
+	});
+
+	this.Then(/^an edit box should be shown$/, function (callback) {
+		// Write code here that turns the phrase above into concrete actions
+		callback.pending();
+	});
+
+	this.Then(/^next button should be enabled$/, function (callback) {
+		// Write code here that turns the phrase above into concrete actions
+		callback.pending();
+	});
+
 
 
 };
